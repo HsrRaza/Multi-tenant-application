cc87dfdcad2abbd2616284bd5f5c5ba84382c7c3
HsrRaza <hr.raza04@gmail.com>
2026-06-10 16:23:52 +0530

    setup and dockerize the database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const port = 3000;
+// middlewares
+app.use(express.json());
+app.use(cors);
+
+
+//routes 
+
+// error handling;
+
+
+
+const port = process.env.PORT|| 3000;
+
+app.get("/", (req, res)=>{
+    res.send("hello world");
+})
 
 app.listen(port, ()=>{
     console.log(`server is runing on ${port}`);
