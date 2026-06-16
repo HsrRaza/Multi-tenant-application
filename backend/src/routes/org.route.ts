7cf3d0a660aca05af9362df5bb91acc7a8cbb058
HsrRaza <hr.raza04@gmail.com>
2026-06-16 20:58:16 +0530

    orgs route

--- backend/src/routes/org.route.ts
+++ backend/src/routes/org.route.ts
@@ -1,5 +1,7 @@
 import express from "express"
+import { authenticate } from "../middleware/auth.middleware.js";
+import { createOrganization } from "../controllers/orgs.controller.js";
 
 const router = express.Router();
 
-router.post("/orgs", )
+router.post("/orgs",authenticate, createOrganization )
